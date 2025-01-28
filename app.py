df6277c9b15828f3fa75ddc1ec4038a005e7d5d6
zeini <23025@supnum.mr>
2025-01-28 23:36:12 +0000

    Mise à jour de l'application Flask

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({"message": "Hello, World!"})
-
+@app.route('/index')
+def index():
+    return 'welcom to index'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
